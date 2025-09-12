9721844fa3878f80f1da986bd0490838d0f4c61c
AjayKumbham <ajaygoud.kumbham@gmail.com>
2025-09-12 18:23:16 +0530

    Add 'Contact' link to navigation in Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,6 +10,7 @@ const Header: React.FC = () => {
     { name: 'Home', href: '/' },
     { name: 'Blog', href: '/blog' },
     { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
   ];
 
   const isActive = (href: string) => location.pathname === href;
